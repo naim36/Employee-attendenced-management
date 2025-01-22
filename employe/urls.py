b9c2e5940bf5cc91a109571606a8aebc2b59f027
naim36 <naimabu8690@gmail.com>
2025-01-22 22:51:18 +0600

    trying

--- employe/urls.py
+++ employe/urls.py
@@ -22,4 +22,5 @@ from Erecord import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
+    path('registration/', views.registration, name = 'registration'),
 ]
